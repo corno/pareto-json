831e2bbdb6d76098969006bec44ed4cbf7229661
Corno Schraverus <corno@schraverus.com>
2025-07-28 18:42:19 +0200

    first commit

---
+++ src/serializers/json.ts
@@ -0,0 +1,27 @@
+import * as pa from 'exupery-core-alg'
+
+
+import * as s_in from "../generated/interface/schemas/json/unconstrained"
+
+import * as t_fountain_pen from "../transformations/json/fountain_pen_block"
+
+import * as s_fp from "pareto-fountain-pen/dist/serialize/block"
+
+export const Document = (
+    $: s_in.Document,
+    $p: {
+        'indentation': string
+        'newline': string
+    }
+): string => {
+    return s_fp.Block(
+        t_fountain_pen.Document(
+            $,
+        ),
+        {
+            'indentation': $p.indentation,
+            'newline': $p['newline'],
+        }
+    )
+
+}
---
+++ src/transformations/json/fountain_pen_block.ts
@@ -0,0 +1,77 @@
+import * as _ea from 'exupery-core-alg'
+
+import * as s_in from "../../generated/interface/schemas/json/unconstrained"
+import * as s_out from "pareto-fountain-pen/dist/generated/interface/schemas/block/unconstrained"
+
+import {
+    b, l, block
+} from "pareto-fountain-pen/dist/shorthands/block"
+
+
+import { impure } from "pareto-standard-operations"
+
+const op = {
+    'enrich list elements with position information': impure.list['enrich with position information'],
+    'serialize with quote delimiter': impure.text['serialize with quote delimiter'],
+    'dictionary to list': impure.dictionary['to list, sorted by code point']
+}
+
+
+const String = (
+    $: string //FIX should have been a schema type
+): s_out.Line_Part => l.snippet(op['serialize with quote delimiter']($))
+
+
+export const Value = ($: s_in.Value): s_out.Line_Part => {
+    return _ea.cc($, ($) => {
+        switch ($[0]) {
+            case 'object': return _ea.ss($, ($) => l.sub([
+                l.snippet("{"),
+                l.indent([
+                    _ea.cc($, ($): s_out.Block_Part => {
+                        switch ($[0]) {
+                            case 'dictionary': return _ea.ss($, ($) => b.sub_decorated(op['enrich list elements with position information'](op['dictionary to list']($)).map(($) => b.nested_line([
+                                String($.value.key),
+                                l.snippet(": "),
+                                Value($.value.value),
+                                $['is last'] ? l.nothing() : l.snippet(","),
+                            ]))))
+                            case 'key value array': return _ea.ss($, ($) => b.sub_decorated(op['enrich list elements with position information']($).map(($) => b.nested_line([
+                                String($.value.key),
+                                l.snippet(": "),
+                                Value($.value.value),
+                                $['is last'] ? l.nothing() : l.snippet(", "),
+                            ]))))
+                            default: return _ea.au($[0])
+                        }
+                    }),
+                ]),
+                l.snippet("}"),
+            ]))
+            case 'array': return _ea.ss($, ($) => _ea.cc($, ($) => l.sub([
+                l.snippet("["),
+                l.sub_decorated(op['enrich list elements with position information']($).map(($) => l.sub([
+                    Value($.value),
+                    $['is last'] ? l.nothing() : l.snippet(", "),
+                ]))),
+                l.snippet("]"),
+            ])))
+            case 'null': return _ea.ss($, ($) => l.snippet("null"))
+            case 'boolean': return _ea.ss($, ($) => l.snippet($ ? "true" : "false"))
+            case 'null': return _ea.ss($, ($) => l.snippet("null"))
+            case 'number': return _ea.ss($, ($) => _ea.cc($, ($) => {
+                switch ($[0]) {
+                    case 'integer': return _ea.ss($, ($) => l.snippet("FIXME INTEGER"))
+                    case 'float': return _ea.ss($, ($) => l.snippet("FIXME FLOAT"))
+                    default: return _ea.au($[0])
+                }
+            }))
+            case 'string': return _ea.ss($, ($) => String($))
+            default: return _ea.au($[0])
+        }
+    })
+}
+
+export const Document = ($: s_in.Document): s_out.Block => block([b.nested_line([
+    Value($),
+])])
