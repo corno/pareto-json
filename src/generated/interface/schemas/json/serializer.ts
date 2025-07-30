e6fcf811cb8c323c91b1ad50525e6afb7f89bd9a
Corno Schraverus <corno@schraverus.com>
2025-07-30 23:14:19 +0200

    added serializers

--- src/generated/interface/schemas/json/serializer.ts
+++ src/generated/interface/schemas/json/serializer.ts
@@ -5,31 +5,106 @@ import * as _i_out from "../../core/astn_target"
 
 // **** TYPES
 
-export type _T_Document = (
+export type _T_Value_Serializers = {
+    readonly 'boolean': (
+        $$_: boolean,
+        $$_p: null,
+    ) => string
+    readonly 'custom numbers': {
+        readonly 'Position': (
+            $$_: number,
+            $$_p: null,
+        ) => string
+    }
+    readonly 'default number': (
+        $$_: number,
+        $$_p: null,
+    ) => string
+}
+
+export type _T_s_Document = (
     $$_: _i_in._T_Document,
-    $$_p: null,
+    $$_p: {
+        readonly 'value serializers': _T_Value_Serializers
+    },
 ) => _i_out._T_Value
 
-export type _T_Value = (
+export type _T_s_Value = (
     $$_: _i_in._T_Value,
-    $$_p: null,
+    $$_p: {
+        readonly 'value serializers': _T_Value_Serializers
+    },
 ) => _i_out._T_Value
 
 // **** FRIENDLY NAMES FOR THE GLOBAL TYPES
 
-export type Document = _T_Document
+export type Value_Serializers = _T_Value_Serializers
 
-export type Value = _T_Value
+export type s_Document = _T_s_Document
+
+export type s_Value = _T_s_Value
 
 // **** ALIASES FOR NESTED TYPE WITH PREFIXED ROOT NAMES
 
-export namespace _T_Document {
+export namespace _T_Value_Serializers {
+    
+    export namespace _boolean {
+        export type CONTEXT = boolean
+        
+        export namespace PARAMS {
+        }
+        export type RESULT = string
+    }
+    export type _boolean = (
+        $$_: boolean,
+        $$_p: null,
+    ) => string
+    
+    export namespace custom_numbers {
+        
+        export namespace Position {
+            export type CONTEXT = number
+            
+            export namespace PARAMS {
+            }
+            export type RESULT = string
+        }
+        export type Position = (
+            $$_: number,
+            $$_p: null,
+        ) => string
+    }
+    export type custom_numbers = {
+        readonly 'Position': (
+            $$_: number,
+            $$_p: null,
+        ) => string
+    }
+    
+    export namespace default_number {
+        export type CONTEXT = number
+        
+        export namespace PARAMS {
+        }
+        export type RESULT = string
+    }
+    export type default_number = (
+        $$_: number,
+        $$_p: null,
+    ) => string
+}
+
+export namespace _T_s_Document {
     
     export namespace CONTEXT {
     }
     export type CONTEXT = _i_in._T_Document
     
     export namespace PARAMS {
+        
+        export namespace value_serializers {
+        }
+        export type value_serializers = _T_Value_Serializers
     }
     
     export namespace RESULT {
@@ -37,13 +112,17 @@ export namespace _T_Document {
     export type RESULT = _i_out._T_Value
 }
 
-export namespace _T_Value {
+export namespace _T_s_Value {
     
     export namespace CONTEXT {
     }
     export type CONTEXT = _i_in._T_Value
     
     export namespace PARAMS {
+        
+        export namespace value_serializers {
+        }
+        export type value_serializers = _T_Value_Serializers
     }
     
     export namespace RESULT {
@@ -53,13 +132,65 @@ export namespace _T_Value {
 
 // *** ALIASES FOR NESTED TYPES
 
-export namespace Document {
+export namespace Value_Serializers {
+    
+    export namespace _boolean {
+        export type CONTEXT = boolean
+        
+        export namespace PARAMS {
+        }
+        export type RESULT = string
+    }
+    export type _boolean = (
+        $$_: boolean,
+        $$_p: null,
+    ) => string
+    
+    export namespace custom_numbers {
+        
+        export namespace Position {
+            export type CONTEXT = number
+            
+            export namespace PARAMS {
+            }
+            export type RESULT = string
+        }
+        export type Position = (
+            $$_: number,
+            $$_p: null,
+        ) => string
+    }
+    export type custom_numbers = {
+        readonly 'Position': (
+            $$_: number,
+            $$_p: null,
+        ) => string
+    }
+    
+    export namespace default_number {
+        export type CONTEXT = number
+        
+        export namespace PARAMS {
+        }
+        export type RESULT = string
+    }
+    export type default_number = (
+        $$_: number,
+        $$_p: null,
+    ) => string
+}
+
+export namespace s_Document {
     
     export namespace CONTEXT {
     }
     export type CONTEXT = _i_in._T_Document
     
     export namespace PARAMS {
+        
+        export namespace value_serializers {
+        }
+        export type value_serializers = _T_Value_Serializers
     }
     
     export namespace RESULT {
@@ -67,13 +198,17 @@ export namespace Document {
     export type RESULT = _i_out._T_Value
 }
 
-export namespace Value {
+export namespace s_Value {
     
     export namespace CONTEXT {
     }
     export type CONTEXT = _i_in._T_Value
     
     export namespace PARAMS {
+        
+        export namespace value_serializers {
+        }
+        export type value_serializers = _T_Value_Serializers
     }
     
     export namespace RESULT {
