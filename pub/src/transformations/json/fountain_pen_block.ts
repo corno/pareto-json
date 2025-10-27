e4be38e65a876c314379f5e14c6587463f27617b
Corno Schraverus <corno@schraverus.com>
2025-10-27 23:25:12 +0100

    .

--- pub/src/transformations/json/fountain_pen_block.ts
+++ pub/src/transformations/json/fountain_pen_block.ts
@@ -12,49 +12,49 @@ import { $$ as op_dictionary_to_list } from "pareto-standard-operations/dist/ope
 
 const String = (
     $: string //FIX should have been a schema type
-): d_out.Line_Part => sh.l.snippet(op_serialize_with_quote_delimiter($))
+): d_out.Block_Part => sh.b.snippet(op_serialize_with_quote_delimiter($))
 
-export const Value = ($: d_in.Value): d_out.Line_Part => {
+export const Value = ($: d_in.Value): d_out.Block_Part => {
     return _ea.cc($, ($) => {
         switch ($[0]) {
-            case 'object': return _ea.ss($, ($) => sh.l.sub([
-                sh.l.snippet("{"),
-                sh.l.indent([
+            case 'object': return _ea.ss($, ($) => sh.b.sub([
+                sh.b.snippet("{"),
+                sh.b.indent([
                     _ea.cc($, ($): d_out.Group_Part => {
                         switch ($[0]) {
-                            case 'dictionary': return _ea.ss($, ($) => sh.g.sub(op_enrich_list_elements_with_position_information(op_dictionary_to_list($)).map(($) => sh.g.nested_line([
+                            case 'dictionary': return _ea.ss($, ($) => sh.g.sub(op_enrich_list_elements_with_position_information(op_dictionary_to_list($)).map(($) => sh.g.nested_block([
                                 String($.value.key),
-                                sh.l.snippet(": "),
+                                sh.b.snippet(": "),
                                 Value($.value.value),
-                                $['is last'] ? sh.l.nothing() : sh.l.snippet(","),
+                                $['is last'] ? sh.b.nothing() : sh.b.snippet(","),
                             ]))))
-                            case 'key value array': return _ea.ss($, ($) => sh.g.sub(op_enrich_list_elements_with_position_information($).map(($) => sh.g.nested_line([
+                            case 'key value array': return _ea.ss($, ($) => sh.g.sub(op_enrich_list_elements_with_position_information($).map(($) => sh.g.nested_block([
                                 String($.value.key),
-                                sh.l.snippet(": "),
+                                sh.b.snippet(": "),
                                 Value($.value.value),
-                                $['is last'] ? sh.l.nothing() : sh.l.snippet(", "),
+                                $['is last'] ? sh.b.nothing() : sh.b.snippet(", "),
                             ]))))
                             default: return _ea.au($[0])
                         }
                     }),
                 ]),
-                sh.l.snippet("}"),
+                sh.b.snippet("}"),
             ]))
-            case 'array': return _ea.ss($, ($) => _ea.cc($, ($) => sh.l.sub([
-                sh.l.snippet("["),
-               sh.l.sub(op_enrich_list_elements_with_position_information($).map(($) => sh.l.sub([
+            case 'array': return _ea.ss($, ($) => _ea.cc($, ($) => sh.b.sub([
+                sh.b.snippet("["),
+               sh.b.sub(op_enrich_list_elements_with_position_information($).map(($) => sh.b.sub([
                     Value($.value),
-                    $['is last'] ? sh.l.nothing() : sh.l.snippet(", "),
+                    $['is last'] ? sh.b.nothing() : sh.b.snippet(", "),
                 ]))),
-                sh.l.snippet("]"),
+                sh.b.snippet("]"),
             ])))
-            case 'null': return _ea.ss($, ($) => sh.l.snippet("null"))
-            case 'boolean': return _ea.ss($, ($) => sh.l.snippet($ ? "true" : "false"))
-            case 'null': return _ea.ss($, ($) => sh.l.snippet("null"))
+            case 'null': return _ea.ss($, ($) => sh.b.snippet("null"))
+            case 'boolean': return _ea.ss($, ($) => sh.b.snippet($ ? "true" : "false"))
+            case 'null': return _ea.ss($, ($) => sh.b.snippet("null"))
             case 'number': return _ea.ss($, ($) => _ea.cc($, ($) => {
                 switch ($[0]) {
-                    case 'integer': return _ea.ss($, ($) => sh.l.snippet("FIXME INTEGER"))
-                    case 'float': return _ea.ss($, ($) => sh.l.snippet("FIXME FLOAT"))
+                    case 'integer': return _ea.ss($, ($) => sh.b.snippet("FIXME INTEGER"))
+                    case 'float': return _ea.ss($, ($) => sh.b.snippet("FIXME FLOAT"))
                     default: return _ea.au($[0])
                 }
             }))
@@ -64,6 +64,6 @@ export const Value = ($: d_in.Value): d_out.Line_Part => {
     })
 }
 
-export const Document = ($: d_in.Document): d_out.Group => sh.group([ sh.g.nested_line([
+export const Document = ($: d_in.Document): d_out.Group => sh.group([ sh.g.nested_block([
     Value($),
 ])])
