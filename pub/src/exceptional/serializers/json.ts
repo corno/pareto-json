f3399440759e9f1165974b967a4eb7acf227fd95
Corno Schraverus <corno@schraverus.com>
2025-10-29 21:49:05 +0100

    u2l

--- pub/src/exceptional/serializers/json.ts
+++ pub/src/exceptional/serializers/json.ts
@@ -3,7 +3,7 @@ import * as pa from 'exupery-core-alg'
 
 import * as d_in from "../../interface/generated/pareto/schemas/json/data_types/source"
 
-import * as t_fountain_pen from "../../implementation/transformations/json/fountain_pen_block"
+import * as t_fountain_pen from "../../implementation/algorithms/transformations/json/fountain_pen_block"
 
 import * as s_fp from "pareto-fountain-pen/dist/exceptional/serialize/block"
 
