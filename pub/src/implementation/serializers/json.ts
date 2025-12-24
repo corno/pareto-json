92bcf9078493ddab2d36e0c577564bfeeeca450a
Corno Schraverus <corno@schraverus.com>
2025-12-24 23:25:21 +0100

    ...

--- pub/src/implementation/serializers/json.ts
+++ pub/src/implementation/serializers/json.ts
@@ -1,7 +1,7 @@
 
 import * as d_in from "../../interface/generated/pareto/schemas/json/data_types/source"
 
-import * as t_fountain_pen from "../../implementation/algorithms/transformations/json/fountain_pen_block"
+import * as t_fountain_pen from "../algorithms/transformations/json/fountain_pen_block"
 
 import * as s_fp from "pareto-fountain-pen/dist/exceptional/serialize/block"
 
