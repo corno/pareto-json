9255e4cb628c26bfce4bc5ff54a53a393dbe67cb
Corno Schraverus <corno@schraverus.com>
2025-12-25 21:21:37 +0100

    ...

--- pub/src/implementation/algorithms/operations/impure/text/serialize_string.ts
+++ pub/src/implementation/algorithms/operations/impure/text/serialize_string.ts
@@ -2,8 +2,6 @@ import * as _et from 'exupery-core-types'
 import * as _ea from 'exupery-core-alg'
 
 import { $$ as op_escape_character } from "pareto-standard-operations/dist/implementation/operations/impure/text/escape_character"
-import { Signature } from "../../../../../interface/algorithms/operations/impure/text/serialize_string"
-
 
 export const $$ = ($: string): string => '"'
     + op_escape_character(
--- pub/src/implementation/serializers/schemas/json.ts
+++ pub/src/implementation/serializers/schemas/json.ts
@@ -1,7 +1,7 @@
 
-import * as d_in from "../../interface/generated/pareto/schemas/json/data_types/source"
+import * as d_in from "../../../interface/generated/pareto/schemas/json/data_types/source"
 
-import * as t_fountain_pen from "../algorithms/transformations/json/fountain_pen_block"
+import * as t_fountain_pen from "../../transformers/schemas/json/fountain_pen_block"
 
 import * as s_fp from "pareto-fountain-pen/dist/implementation/serializers/block"
 
