8b8cd46a89b811f8aa0288a20fb80023b6e934e7
Corno Schraverus <corno@schraverus.com>
2025-12-25 22:01:55 +0100

    ...

--- pub/src/implementation/transformers/schemas/json/fountain_pen_block.ts
+++ pub/src/implementation/transformers/schemas/json/fountain_pen_block.ts
@@ -6,7 +6,7 @@ import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/sche
 import * as sh from "pareto-fountain-pen/dist/shorthands/block"
 
 import { $$ as op_enrich_list_elements_with_position_information } from "pareto-standard-operations/dist/implementation/operations/impure/list/enrich_with_position_information"
-import { $$ as op_serialize_with_quote_delimiter } from "../../../algorithms/operations/impure/text/serialize_string"
+import { $$ as op_serialize_with_quote_delimiter } from "../../../serializers/primitives/text/quoted"
 import { $$ as op_dictionary_to_list } from "pareto-standard-operations/dist/implementation/operations/impure/dictionary/to_list_sorted_by_insertion"
 
 
