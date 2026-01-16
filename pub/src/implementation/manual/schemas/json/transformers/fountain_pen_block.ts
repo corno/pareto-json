4693501684d884339f3220159ee3ad76fe634776
Corno Schraverus <corno@schraverus.com>
2026-01-16 09:20:56 +0100

    ...

--- pub/src/implementation/manual/schemas/json/transformers/fountain_pen_block.ts
+++ pub/src/implementation/manual/schemas/json/transformers/fountain_pen_block.ts
@@ -2,7 +2,7 @@ import * as _p from 'pareto-core-transformer'
 
 //data types
 import * as d_in from "../../../../../interface/generated/pareto/schemas/json/data"
-import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/schemas/block/data_types/target"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/pareto/schemas/block/data"
 
 //shorthands
 import * as sh from "pareto-fountain-pen/dist/shorthands/block"
