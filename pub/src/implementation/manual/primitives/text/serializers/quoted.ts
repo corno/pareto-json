e8a6a49f0f4d2685a26edaa14a74edb225382eea
Corno Schraverus <corno@schraverus.com>
2026-01-22 13:52:28 +0100

    ...

--- pub/src/implementation/manual/primitives/text/serializers/quoted.ts
+++ pub/src/implementation/manual/primitives/text/serializers/quoted.ts
@@ -7,50 +7,50 @@ export const $$ = ($: string): string => '"'
         _pd.list.from_text($, ($) => $).__for_each(($) => {
             switch ($) {
                 case 0x2F: // slash (\/)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x2F) // /
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x2F) // /
                     break
                 case 0x22: // " (\")
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x22) // "
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x22) // "
                     break
             
                 case 0x5C: // \ (\\)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x5C) // \
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x5C) // \
                     break
             
                 case 0x08: // backspace (\b)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x62) // b
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x62) // b
                     break
             
                 case 0x0C: // form feed (\f)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x66) // f
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x66) // f
                     break
             
                 case 0x0A: // line feed (\n)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x6E) // n
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x6E) // n
                     break
             
                 case 0x0D: // carriage return (\r)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x72) // r
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x72) // r
                     break
             
                 case 0x09: // horizontal tab (\t)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x74) // t
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x74) // t
                     break
             
                 case 0x0B: // vertical tab (\v)
-                    $i['add character'](0x5C) // \
-                    $i['add character'](0x76) // v
+                    $i.add_character(0x5C) // \
+                    $i.add_character(0x76) // v
                     break
                 default: {
-                   $i['add character']($)
+                   $i.add_character($)
                 }
         
             }
