93729289a4e700ddeaa6a53e409dcaf9bb35ca76
Corno Schraverus <corno@schraverus.com>
2026-01-26 12:42:47 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/json/deserialize.ts
@@ -0,0 +1,16 @@
+
+import * as _p from "pareto-core/dist/deserializer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/json/deserialize"
+
+import * as v_deserialize from "astn-core/dist/implementation/manual/schemas/parse_tree/deserializers"
+
+import * as v_unmarshall from "./unmarshall"
+export const Value: t_signatures.Value = ($, abort) => v_unmarshall.Value(v_deserialize.Document($, () => abort(['tbd', null]), {
+    'uri': "FIXME URI",
+    'tab size': 4,
+}).content, abort)
+export const Document: t_signatures.Document = ($, abort) => v_unmarshall.Document(v_deserialize.Document($, () => abort(['tbd', null]), {
+    'uri': "FIXME URI",
+    'tab size': 4,
+}).content, abort)
