1ef5abbfe1812331bddc4c25a7d987e84613e2ed
Corno Schraverus <corno@schraverus.com>
2026-01-26 11:27:27 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/json/serialize.ts
@@ -0,0 +1,10 @@
+
+import * as _p from "pareto-core/dist/serializer"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/json/serialize"
+
+import * as v_serialize from "astn-core/dist/implementation/manual/schemas/sealed_target/serializers"
+
+import * as v_marshall from "./marshall"
+export const Value: t_signatures.Value = ($,) => v_serialize.Document(v_marshall.Value($))
+export const Document: t_signatures.Document = ($,) => v_serialize.Document(v_marshall.Document($))
