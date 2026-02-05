82ba11963fc8ad5fce2c37c5a4c3748ef6216fdb
Corno Schraverus <corno@schraverus.com>
2026-02-05 20:23:36 +0100

    pdt: ...

---
+++ pub/src/implementation/generated/liana/schemas/json/transformers/fountain_pen.ts
@@ -0,0 +1,20 @@
+    
+    import * as _p from "pareto-core/dist/expression"
+    
+    import * as t_signatures from "../../../../../../interface/generated/liana/schemas/json/serialize"
+    
+    import * as v_serialize from "astn-core/dist/implementation/manual/schemas/sealed_target/transformers/fountain_pen"
+    
+    import * as v_marshall from "./astn_sealed_target"
+    
+    export const Document: t_signatures.Document = ($) => v_serialize.Document(
+        v_marshall.Document(
+            $,
+        ),
+    )
+    
+    export const Value: t_signatures.Value = ($) => v_serialize.Document(
+        v_marshall.Value(
+            $,
+        ),
+    )
