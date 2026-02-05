fc05e700c4264d2453356a125d8e1ad4a2242fd7
Corno Schraverus <corno@schraverus.com>
2026-02-05 19:22:04 +0100

    pdt: ...

---
+++ pub/src/implementation/generated/liana/schemas/json/transformers/astn_sealed_target.ts
@@ -0,0 +1,186 @@
+    
+    import * as _p from "pareto-core/dist/expression"
+    
+    import _p_change_context from "pareto-core/dist/_p_change_context"
+    
+    import _p_text_from_list from "pareto-core/dist/_p_text_from_list"
+    
+    import * as t_signatures from "../../../../../../interface/generated/liana/schemas/json/marshall"
+    
+    import * as t_out from "astn-core/dist/interface/generated/liana/schemas/sealed_target/data"
+    
+    import * as v_serialize_number from "liana-core/dist/implementation/manual/primitives/integer/serializers/decimal"
+    
+    import * as v_serialize_boolean from "liana-core/dist/implementation/manual/primitives/boolean/serializers/true_false"
+    
+    export const Document: t_signatures.Document = ($) => Value(
+        $,
+    )
+    
+    export const Value: t_signatures.Value = ($) => ['state', _p.decide.state(
+        $,
+        ($): t_out.Value.state => {
+            switch ($[0]) {
+                case 'array':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'array',
+                            'value': ['list', _p.list.map(
+                                $,
+                                ($) => Value(
+                                    $,
+                                ),
+                            )],
+                        }),
+                    )
+                case 'object':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'object',
+                            'value': ['state', _p.decide.state(
+                                $,
+                                ($): t_out.Value.state => {
+                                    switch ($[0]) {
+                                        case 'key value array':
+                                            return _p.ss(
+                                                $,
+                                                ($) => ({
+                                                    'option': 'key value array',
+                                                    'value': ['list', _p.list.map(
+                                                        $,
+                                                        ($) => ['group', ['verbose', _p.dictionary.literal(
+                                                            {
+                                                                'key': _p_change_context(
+                                                                    $['key'],
+                                                                    ($) => ['text', {
+                                                                        'delimiter': ['quote', null],
+                                                                        'value': $,
+                                                                    }],
+                                                                ),
+                                                                'value': _p_change_context(
+                                                                    $['value'],
+                                                                    ($) => Value(
+                                                                        $,
+                                                                    ),
+                                                                ),
+                                                            },
+                                                        )]],
+                                                    )],
+                                                }),
+                                            )
+                                        case 'dictionary':
+                                            return _p.ss(
+                                                $,
+                                                ($) => ({
+                                                    'option': 'dictionary',
+                                                    'value': ['dictionary', _p.dictionary.map(
+                                                        $,
+                                                        ($, id) => Value(
+                                                            $,
+                                                        ),
+                                                    )],
+                                                }),
+                                            )
+                                        default:
+                                            return _p.au(
+                                                $[0],
+                                            )
+                                    }
+                                },
+                            )],
+                        }),
+                    )
+                case 'number':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'number',
+                            'value': ['state', _p.decide.state(
+                                $,
+                                ($): t_out.Value.state => {
+                                    switch ($[0]) {
+                                        case 'integer':
+                                            return _p.ss(
+                                                $,
+                                                ($) => ({
+                                                    'option': 'integer',
+                                                    'value': ['text', {
+                                                        'delimiter': ['none', null],
+                                                        'value': _p_text_from_list(
+                                                            v_serialize_number.serialize(
+                                                                $,
+                                                            ),
+                                                            ($) => $,
+                                                        ),
+                                                    }],
+                                                }),
+                                            )
+                                        case 'float':
+                                            return _p.ss(
+                                                $,
+                                                ($) => ({
+                                                    'option': 'float',
+                                                    'value': ['text', {
+                                                        'delimiter': ['none', null],
+                                                        'value': _p_text_from_list(
+                                                            v_serialize_number.serialize(
+                                                                $,
+                                                            ),
+                                                            ($) => $,
+                                                        ),
+                                                    }],
+                                                }),
+                                            )
+                                        default:
+                                            return _p.au(
+                                                $[0],
+                                            )
+                                    }
+                                },
+                            )],
+                        }),
+                    )
+                case 'string':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'string',
+                            'value': ['text', {
+                                'delimiter': ['quote', null],
+                                'value': $,
+                            }],
+                        }),
+                    )
+                case 'boolean':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'boolean',
+                            'value': ['text', {
+                                'delimiter': ['none', null],
+                                'value': _p_text_from_list(
+                                    v_serialize_boolean.serialize(
+                                        $,
+                                    ),
+                                    ($) => $,
+                                ),
+                            }],
+                        }),
+                    )
+                case 'null':
+                    return _p.ss(
+                        $,
+                        ($) => ({
+                            'option': 'null',
+                            'value': ['nothing', null],
+                        }),
+                    )
+                default:
+                    return _p.au(
+                        $[0],
+                    )
+            }
+        },
+    )]
