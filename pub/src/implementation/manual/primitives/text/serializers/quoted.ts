0f8c68b1e60cf263850abff98f7d8f9d41315c76
Corno Schraverus <corno@schraverus.com>
2026-02-12 22:00:44 +0100

    pdt: ...

--- pub/src/implementation/manual/primitives/text/serializers/quoted.ts
+++ pub/src/implementation/manual/primitives/text/serializers/quoted.ts
@@ -3,7 +3,7 @@ import * as _pi from 'pareto-core/dist/interface'
 import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 
 //data types
-import * as d_out from "pareto-fountain-pen/dist/interface/to_be_generated/list_of_characters"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
 
 
 export const $$ = (
--- pub/src/interface/signatures.ts
+++ pub/src/interface/signatures.ts
@@ -1,6 +1,6 @@
 import * as _pi from 'pareto-core/dist/interface'
 
-import * as d_out from "pareto-fountain-pen/dist/interface/to_be_generated/list_of_characters"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
 
 export namespace deserializers {
 
