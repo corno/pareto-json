0fbf569c012ebdd7550c9a6fc5da17c9de885d60
Corno Schraverus <corno@schraverus.com>
2026-05-17 13:59:59 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/refiners/primitives/list_of_characters.ts
+++ typescript/lib/src/implementation/manual/refiners/primitives/list_of_characters.ts
@@ -1,6 +1,5 @@
 import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 import * as _p from 'pareto-core/dist/assign'
-import _p_unreachable_code_path from 'pareto-core/dist/_p_unreachable_code_path'
 import _p_list_build_deprecated from 'pareto-core/dist/_p_list_build_deprecated'
 import * as _pi from 'pareto-core/dist/interface'
 
