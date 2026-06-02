2e21b993c13e952b893cc2d466e49c183cdf6442
Corno Schraverus <corno@schraverus.com>
2026-06-02 12:03:16 +0200

    pdt: ...

--- typescript/lib/src/interface/to_be_generated/json_from_list_of_characters.ts
+++ typescript/lib/src/interface/to_be_generated/json_from_list_of_characters.ts
@@ -1,4 +1,3 @@
-import * as d_location from "astn-core/dist/interface/generated/liana/schemas/location/data"
 import * as d_deserialize from "astn-core/dist/interface/generated/liana/schemas/deserialize_parse_tree/data"
 import * as d_json_from_parse_tree from "./json_from_parse_tree"
 
