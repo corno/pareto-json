cae34d15e5f3baa3cb96f78650119dbf1d79a49e
Corno Schraverus <corno@schraverus.com>
2026-06-02 11:32:57 +0200

    pdt: published version 0.3.54

---
+++ typescript/lib/src/shorthands/json_with_unique_keys.ts
@@ -0,0 +1,22 @@
+import * as _p from 'pareto-core-shorthands/dist/unconstrained'
+
+import * as d_out from "../interface/generated/liana/schemas/json_with_unique_keys/data"
+
+
+export namespace v {
+
+    export const string = (
+        value: string
+    ): d_out.Value => ['string', value]
+
+    export const null_ = (
+    ): d_out.Value => ['null', null]
+
+    export const object = (
+        members: _p.Raw_Or_Normal_Dictionary<d_out.Value.object_.D>
+    ): d_out.Value => ['object', _p.dictionary.literal(members)]
+
+    export const array = (
+        elements: _p.Raw_Or_Normal_List<d_out.Value.array.L>
+    ): d_out.Value => ['array', _p.list.literal(elements)]
+}
