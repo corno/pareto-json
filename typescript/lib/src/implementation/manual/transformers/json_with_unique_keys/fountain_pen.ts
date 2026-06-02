def2247a838cfc5c92bad103150945b77aae0d2d
Corno Schraverus <corno@schraverus.com>
2026-06-02 11:36:58 +0200

    pdt: published version 0.3.55

---
+++ typescript/lib/src/implementation/manual/transformers/json_with_unique_keys/fountain_pen.ts
@@ -0,0 +1,71 @@
+import * as _p from 'pareto-core/dist/assign'
+
+//data types
+import * as d_in from "../../../../interface/generated/liana/schemas/json_with_unique_keys/data"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+
+//shorthands
+import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
+
+//dependencies
+import * as t_primitives_to_loc from "../primitives/list_of_characters"
+
+const String = (
+    $: string //FIX should have been a schema type
+): d_out.Phrase => sh.ph.serialize(t_primitives_to_loc.String($))
+
+export const Value = ($: d_in.Value): d_out.Phrase => _p.decide.state($, ($) => {
+    switch ($[0]) {
+        case 'object': return _p.ss($, ($) => sh.ph.composed([
+            sh.ph.literal("{"),
+            sh.ph.indent(
+                sh.pg.rich(
+                    $.__to_list(($, id) => sh.sentence([
+                        String(id),
+                        sh.ph.literal(": "),
+                        Value($)
+                    ])),
+                    null,
+                    false,
+                    null,
+                    sh.ph.literal(","),
+                    null,
+                ),
+
+            ),
+            sh.ph.literal("}"),
+        ]))
+        case 'array': return _p.ss($, ($) => sh.ph.composed([
+            sh.ph.literal("["),
+            sh.ph.rich(
+                $.__l_map(($) => sh.ph.composed([
+                    sh.ph.literal(" "),
+                    Value($),
+                ])),
+                sh.ph.nothing(),
+                sh.ph.nothing(),
+                sh.ph.literal(","),
+                sh.ph.nothing(),
+            ),
+            sh.ph.literal(" ]"),
+        ]))
+        case 'null': return _p.ss($, ($) => sh.ph.literal("null"))
+        case 'boolean': return _p.ss($, ($) => sh.ph.literal($ ? "true" : "false"))
+        case 'null': return _p.ss($, ($) => sh.ph.literal("null"))
+        case 'number': return _p.ss($, ($) => _p.decide.state($, ($) => {
+            switch ($[0]) {
+                case 'integer': return _p.ss($, ($) => sh.ph.serialize(t_primitives_to_loc.Fractional_Decimal($, { 'number of fractional digits': 0 })))
+                case 'float': return _p.ss($, ($) => sh.ph.serialize(t_primitives_to_loc.Float($, { 'digits': 15 })))
+                default: return _p.au($[0])
+            }
+        }))
+        case 'string': return _p.ss($, ($) => String($))
+        default: return _p.au($[0])
+    }
+})
+
+export const Document = ($: d_in.Document_): d_out.Paragraph => sh.pg.sentences([
+    sh.sentence([
+        Value($)
+    ])
+])
