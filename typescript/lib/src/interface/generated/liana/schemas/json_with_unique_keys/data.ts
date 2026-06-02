8f645d6f2128c4f77fee850febc2966739d3ca45
Corno Schraverus <corno@schraverus.com>
2026-06-02 11:31:16 +0200

    pdt: published version 0.3.53

---
+++ typescript/lib/src/interface/generated/liana/schemas/json_with_unique_keys/data.ts
@@ -0,0 +1,55 @@
+
+import * as _pi from 'pareto-core/dist/interface'
+
+export type Document_ = Value_
+
+export namespace Value_ {
+    
+    export namespace array {
+        
+        export type L = Value_
+        
+    }
+    
+    export type array = _pi.List<array.L>
+    
+    export namespace object_ {
+        
+        export type D = Value_
+        
+    }
+    
+    export type object_ = _pi.Dictionary<object_.D>
+    
+    export namespace number_ {
+        
+        export type integer = number
+        
+        export type float = number
+        
+    }
+    
+    export type number_ = 
+        | readonly ['integer', number_.integer]
+        | readonly ['float', number_.float]
+    
+    export type string_ = string
+    
+    export type boolean_ = boolean
+    
+    export type null_ = null
+    
+}
+
+export type Value_ = 
+    | readonly ['array', Value_.array]
+    | readonly ['object', Value_.object_]
+    | readonly ['number', Value_.number_]
+    | readonly ['string', Value_.string_]
+    | readonly ['boolean', Value_.boolean_]
+    | readonly ['null', Value_.null_]
+
+export { 
+    Document_ as Document, 
+    Value_ as Value, 
+}
