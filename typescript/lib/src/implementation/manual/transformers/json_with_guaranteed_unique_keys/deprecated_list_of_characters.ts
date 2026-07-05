404295adcbe6cc123c968210f5a38c0b7b6414b0
Corno Schraverus <corno@schraverus.com>
2026-07-05 09:53:15 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/json_with_guaranteed_unique_keys/deprecated_list_of_characters.ts
+++ typescript/lib/src/implementation/manual/transformers/json_with_guaranteed_unique_keys/deprecated_list_of_characters.ts
@@ -5,17 +5,20 @@ import * as d_in from "../../../../interface/generated/liana/schemas/json_with_g
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
 import * as d_function_fp from "pareto-fountain-pen/dist/interface/data/prose_serialize"
 
+export namespace interface_ {
+    export type Value = p_i.Transformer_With_Parameter<
+        d_in.Value,
+        d_out.List_of_Characters,
+        d_function_fp.Parameters
+    >
+}
 
 //dependencies
 import * as t_to_prose from "./prose"
 import * as t_fp_to_characters from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/list_of_characters"
 
 
-export const Value: p_i.Transformer_With_Parameter<
-    d_in.Value,
-    d_out.List_of_Characters,
-    d_function_fp.Parameters
-> = ($, $p) => t_fp_to_characters.Phrase(
+export const Value: interface_.Value = ($, $p) => t_fp_to_characters.Phrase(
     t_to_prose.Value($),
     $p
 )
--- typescript/lib/src/implementation/manual/transformers/json_without_guaranteed_unique_keys/text.ts
+++ typescript/lib/src/implementation/manual/transformers/json_without_guaranteed_unique_keys/text.ts
@@ -3,19 +3,22 @@ import * as p_i from 'pareto-core/dist/interface/transformer'
 //data types
 import * as d_in from "../../../../interface/generated/liana/schemas/json_without_guaranteed_unique_keys/data"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/text/data"
-import * as d_function_fp from "pareto-fountain-pen/dist/interface/data/prose_serialize"
+import * as d_function from "pareto-fountain-pen/dist/interface/data/prose_serialize"
 
+export namespace interface_ {
+    export type Value = p_i.Transformer_With_Parameter<
+        d_in.Value,
+        d_out.Text,
+        d_function.Parameters
+    >
+}
 
 //dependencies
 import * as t_to_prose from "./prose"
-import * as t_fp_to_text from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/text"
+import * as t_prose_to_text from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/text"
 
 
-export const Value: p_i.Transformer_With_Parameter<
-    d_in.Value,
-    d_out.Text,
-    d_function_fp.Parameters
-> = ($, $p) => t_fp_to_text.Phrase(
+export const Value: interface_.Value = ($, $p) => t_prose_to_text.Phrase(
     t_to_prose.Value($),
     $p
 )
