25278c1580b332b62c877049d6d90b83703196aa
Corno Schraverus <corno@schraverus.com>
2026-07-06 16:55:11 +0200

    pdt: ...

--- typescript/lib/src/interface/data/json_y.ts
+++ typescript/lib/src/interface/data/json_y.ts
@@ -2,7 +2,6 @@
 import * as d_astn_location from "astn-core/interface/generated/liana/schemas/location/data"
 
 export type Error = {
-
     'conflicting key': string
     'range': d_astn_location.Range
 }
