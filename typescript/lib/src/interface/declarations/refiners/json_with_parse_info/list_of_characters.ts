3bf3233e7a6587affc8846c6d41775a0cfdd99c3
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:37:41 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/refiners/json_with_parse_info/list_of_characters.ts
+++ typescript/lib/src/interface/declarations/refiners/json_with_parse_info/list_of_characters.ts
@@ -1,8 +1,8 @@
 import type * as p_i from 'pareto-core/interface/refiner'
 
 //data types
-import type * as d_out from "../../../../interface/data/json_with_parse_info.js"
-import type * as d_function from "../../../../interface/data/json_from_list_of_characters_refiner.js"
+import type * as d_out from "../../../data/json_with_parse_info.js"
+import type * as d_function from "../../../data/json_from_list_of_characters_refiner.js"
 import type * as d_in from "pareto-fountain-pen/interface/generated/liana/schemas/list_of_characters/data"
 import type * as d_parse_tree_from_list_of_characters from "astn-core/interface/generated/liana/schemas/deserialize_parse_tree/data"
 
--- typescript/lib/src/interface/declarations/transformers/json_with_guaranteed_unique_keys/text.ts
+++ typescript/lib/src/interface/declarations/transformers/json_with_guaranteed_unique_keys/text.ts
@@ -1,7 +1,7 @@
 import type * as p_i from 'pareto-core/interface/transformer'
 
 //data types
-import type * as d_in from "../../../../interface/generated/liana/schemas/json_with_guaranteed_unique_keys/data.js"
+import type * as d_in from "../../../generated/liana/schemas/json_with_guaranteed_unique_keys/data.js"
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/text/data"
 import type * as d_function_fp from "pareto-fountain-pen/interface/data/prose_serialize"
 
