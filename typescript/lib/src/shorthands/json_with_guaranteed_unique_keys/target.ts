3188dcc455fa02cef0c3d0910cb919ed6a76925c
Corno Schraverus <corno@schraverus.com>
2026-07-17 09:24:40 +0200

    pdt: published version 0.3.91

--- typescript/lib/src/shorthands/json_with_guaranteed_unique_keys/target.ts
+++ typescript/lib/src/shorthands/json_with_guaranteed_unique_keys/target.ts
@@ -2,21 +2,18 @@ import * as p_ from 'pareto-core-shorthands/unconstrained_target'
 
 import type * as s_out from "../../modules/serialization/schemas/with_guaranteed_unique_keys.js"
 
-namespace v {
 
-    export const string = (
-        value: string
-    ): s_out.Value => ['string', value]
+export const string = (
+    value: string
+): s_out.Value => ['string', value]
 
-    export const null_ = (
-    ): s_out.Value => ['null', null]
+export const null_ = (
+): s_out.Value => ['null', null]
 
-    export const object = (
-        members: p_.Normal_Dictionary<s_out.Value.object_.D>
-    ): s_out.Value => ['object', p_.dictionary(members)]
+export const object = (
+    members: p_.Normal_Dictionary<s_out.Value.object_.D>
+): s_out.Value => ['object', p_.dictionary(members)]
 
-    export const array = (
-        elements: p_.Normal_List<s_out.Value.array.L>
-    ): s_out.Value => ['array', p_.list(elements)]
-    
-}
+export const array = (
+    elements: p_.Normal_List<s_out.Value.array.L>
+): s_out.Value => ['array', p_.list(elements)]
