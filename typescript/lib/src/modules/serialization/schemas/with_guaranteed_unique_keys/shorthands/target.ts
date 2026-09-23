1cc7ee4f363ed829a7e94225ca68566187ba2052
Corno Schraverus <corno@schraverus.com>
2026-09-23 10:30:11 +0200

    pdt: ...

--- typescript/lib/src/modules/serialization/schemas/with_guaranteed_unique_keys/shorthands/target.ts
+++ typescript/lib/src/modules/serialization/schemas/with_guaranteed_unique_keys/shorthands/target.ts
@@ -2,18 +2,36 @@ import * as p_ from 'pareto-core-shorthands/unconstrained_target'
 
 import type * as s_out from "../schema.js"
 
+export const array = (
+    elements: p_.Normal_List<s_out.Value.array.L>
+): s_out.Value => ['array', p_.list(elements)]
 
-export const string = (
-    value: string
-): s_out.Value => ['string', value]
+export const boolean = (
+    value: boolean
+): s_out.Value => ['boolean', value]
+
+export const false_ = (
+): s_out.Value => ['boolean', false]
+
+export const integer = (
+    value: number
+): s_out.Value => ['number', ['integer', value]]
+
+export const true_ = (
+): s_out.Value => ['boolean', true]
 
 export const null_ = (
 ): s_out.Value => ['null', null]
+
+export const float = (
+    value: number
+): s_out.Value => ['number', ['float', value]]
 
 export const object = (
     members: p_.Normal_Dictionary<s_out.Value.object_.D>
 ): s_out.Value => ['object', p_.dictionary(members)]
 
-export const array = (
-    elements: p_.Normal_List<s_out.Value.array.L>
-): s_out.Value => ['array', p_.list(elements)]
+export const string = (
+    value: string
+): s_out.Value => ['string', value]
+
